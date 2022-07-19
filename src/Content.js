ea689911584011e34b81ff6509443c32f3d73bcf
Narender Singh Bisht <narendersinghbisht382@gmail.com>
2022-07-19 21:15:27 +0530

    my-portfolio-website

--- src/Content.js
+++ src/Content.js
@@ -1,13 +1,13 @@
 import React, { Suspense} from 'react'
 import {BrowserRouter, Route, Routes } from "react-router-dom";
 import { CircularProgress } from '@mui/material';
-const Navbar = React.lazy(()=>import("./Components/Navbar/Navbar"));
-const Portfolio = React.lazy(()=>import("./Pages/Portfolio/Portfolio"));
-const Resume = React.lazy(()=>import("./Pages/Resume/Resume"));
-const About = React.lazy(()=>import("./Pages/About/About"));
-const Contact = React.lazy(()=>import("./Pages/Contact/Contact"));
-const Home = React.lazy(()=>import("./Pages/Home/Home"));
-const Footer = React.lazy(()=>import("./Components/Footer/Footer"));
+import Navbar from "./Components/Navbar/Navbar";
+import Portfolio from "./Pages/Portfolio/Portfolio";
+import Resume from "./Pages/Resume/Resume";
+import About from "./Pages/About/About";
+import Contact from "./Pages/Contact/Contact";
+import Home from "./Pages/Home/Home";
+import Footer from "./Components/Footer/Footer";
 function Content() {
   return (
     <BrowserRouter>
