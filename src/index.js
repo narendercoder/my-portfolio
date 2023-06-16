b7f5cea2c19762473110387c6d13148850fd731f
Narender Singh Bisht <narendersinghbisht382@gmail.com>
2023-06-16 14:20:34 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <AppProvider>
   <React.StrictMode>
-    <App />
+    <App  />
   </React.StrictMode>
   </AppProvider>
 );
